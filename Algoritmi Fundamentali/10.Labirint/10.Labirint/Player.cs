54f686a8f90cfcafb3b4495241cbd30546f48587
PelleRemus <remusnicolaepelle@gmail.com>
2026-05-05 14:19:59 +0300

    baza algoritmului labirint

---
+++ Algoritmi Fundamentali/10.Labirint/10.Labirint/Player.cs
@@ -0,0 +1,62 @@
+﻿namespace _10.Labirint
+{
+    public static class Player
+    {
+        // Pozitia curenta a jucatorului si pozitia destinatiei
+        public static Point position, destinationPosition;
+        public static Image image = Image.FromFile("../../../Resources/player.png");
+        public static Form1 form;
+        public static PictureBox destination; // Picturebox-ul care reprezinta destinatia jucatorului
+        // Calea reprezentata prin mai multe puncte, pe care trebuie sa o faca jucatorul pentru a ajunge la destinatie
+        public static List<Point> path;
+
+        public static void Init(Form1 f)
+        {
+            form = f;
+            path = new List<Point>();
+            // Apelam metoda pentru a adauga imaginea pe pozitia curenta a jucatorului (pozitia 0, 0)
+            ChangePosition(position);
+        }
+
+        public static void ChangePosition(Point newPosition)
+        {
+            // Intai, stergem imaginea de pe pozitia curenta
+            form.display[position.Y, position.X].pictureBox.Image = null;
+            // Apoi o adaugam pe noua pozitie
+            form.display[newPosition.Y, newPosition.X].pictureBox.Image = image;
+            position = newPosition;
+        }
+        public static void ChangeDestination(MapTile newDestination)
+        {
+            // Intai stergem culoarea aurie de fundal a destinatiei curente
+            if (destination != null)
+                destination.BackColor = Color.ForestGreen;
+
+            // Si apoi o adaugam la destinatia noua
+            newDestination.pictureBox.BackColor = Color.Gold;
+            destinationPosition = new Point(newDestination.column, newDestination.line);
+            destination = newDestination.pictureBox;
+
+            // Reinitializam matricea pentru a incepe din nou de la 0
+            // ca apoi sa putem apela din nou algoritmul lui Lee si acesta sa functioneze
+            form.InitializeMatrixWithWalls();
+            FindPathLee();
+        }
+
+        public static void GoToDestination()
+        {
+            // Daca nu avem niciun punct la care trebuie sa mergem, inseamna ca am ajuns la destinatie
+            if (path.Count == 0)
+                return;
+            // luam urmatorul punct din lista, il stergem din lista, si schimbam pozitia jucatorului la acel punct
+            Point nextPosition = path[0];
+            path.RemoveAt(0);
+            ChangePosition(nextPosition);
+        }
+
+        public static void FindPathLee()
+        {
+
+        }
+    }
+}
---
+++ Algoritmi Fundamentali/10.Labirint/10.Labirint/Queue.cs
@@ -0,0 +1,40 @@
+﻿namespace _10.Labirint
+{
+    public class Queue
+    {
+        public MapTile[] v;
+        public int length;
+
+        // Coada are proprietatile ca se adauga la final si se stearga de la inceput
+        public Queue()
+        {
+            length = 0;
+            v = [];
+        }
+
+        public void Add(MapTile tile)
+        {
+            length++;
+            MapTile[] newV = new MapTile[length];
+            for (int i = 0; i < length - 1; i++)
+            {
+                newV[i] = v[i];
+            }
+            newV[length - 1] = tile;
+            v = newV;
+        }
+
+        public MapTile Remove()
+        {
+            length--;
+            MapTile[] newV = new MapTile[length];
+            for (int i = 0; i < length; i++)
+            {
+                newV[i] = v[i + 1];
+            }
+            MapTile toReturn = v[0];
+            v = newV;
+            return toReturn;
+        }
+    }
+}
